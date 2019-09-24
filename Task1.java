7d25c9496400b5d37879fbf6631f7ea8a2356fa1
18SW144 <55759264+Ehtisham-arch@users.noreply.github.com>
2019-09-24 22:32:46 +0500

    Add files via upload

--- Task1.java
+++ Task1.java
@@ -1,20 +1,8 @@
 class Task1{
 	public static void main(String args[]){
-		String name="Ali Raza";
-		long total_marks=500;
-		int obtained_marks=377;
-		float percentage=obtained_marks*100/total_marks;
-		char grade='A';
-		boolean b=obtained_marks>=33;
-		System.out.println("Name of Student:"+name);
-		System.out.println("Total Marks:"+total_marks);
-		System.out.println("Obtained Marks:"+obtained_marks);
-		System.out.println("Percentage:"+percentage);
-		System.out.println("Grade:"+grade);	
-		System.out.print("Status:");
-		if(b)
-			System.out.println("Pass");
-		else
-			System.out.println("Fail");
+		String name=args[0];
+		String roll_no=args[1];
+		System.out.println("Name:"+name);
+		System.out.println("Roll No:"+roll_no);
 	}
 }
--- Task2.java
+++ Task2.java
@@ -1,8 +1,35 @@
 class Task2{
 	public static void main(String args[]){
-		double var1=50.25;
-		System.out.println("Orignal Value:"+var1);
-		System.out.println("Integer Part:"+(int)var1);
-		System.out.println("Float part:"+(var1%=(int)var1));
+		int a=5,b=6;
+		boolean c,d=true,e=false;
+		//Relational Operator:
+		System.out.println("Relational Operators:");
+		System.out.println();
+		c=a>b;
+		System.out.println(c);
+		c=a<b;
+		System.out.println(c);
+		c=a>=b;
+		System.out.println(c);
+		c=a<=b;
+		System.out.println(c);
+		c=a==b;
+		System.out.println(c);
+		c=a!=b;
+		System.out.println(c);
+		System.out.println();
+		System.out.println("Logical Boolean Operators:");
+		//Logical Boolean Operator:
+		System.out.println();
+		c=d&e;
+		System.out.println(c);
+		c=d|e;
+		System.out.println(c);
+		c=!d;
+		System.out.println(c);
+		c=!e;
+		System.out.println(c);
+		c=d^e;
+		System.out.println(c);
 	}
 }
